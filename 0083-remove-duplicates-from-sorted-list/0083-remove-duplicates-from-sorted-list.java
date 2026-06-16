a35a21500a04325f7712947e2ab2a6756957727e
RAMYASHREE BALASUBRAMANIAN <ramyashree2812@gmail.com>
2026-06-16 14:01:14 +0530

    Time: 0 ms (100%), Space: 45.5 MB (31.65%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,20 +10,17 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        // Start from the head
-        ListNode current = head;
         
-        // Traverse the list
-        while (current != null && current.next != null) {
-            if (current.val == current.next.val) {
-                // Skip the duplicate node
-                current.next = current.next.next;
-            } else {
-                // Move to the next distinct element
-                current = current.next;
+        ListNode cu= head;
+        while(cu!=null && cu.next!=null){
+            if(cu.val==cu.next.val){
+                cu.next=cu.next.next;
+            }
+            else{
+                cu=cu.next;
             }
         }
-        
         return head;
+        
     }
 }
