112287fcb42609a3e7e36a559b63b827ad1f89b1
RAMYASHREE BALASUBRAMANIAN <ramyashree2812@gmail.com>
2026-07-15 10:02:07 +0530

    Time: 4 ms (38.44%), Space: 43.3 MB (41.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-     Stack<Character>st=new Stack<>();
-     for(int i=0;i<s.length();i++){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
         char t=s.charAt(i);
-        if(t=='{'||t=='['||t=='('){
-            st.push(t);
-             }
+        if(t=='('|| t=='['|| t=='{'){
+        st.push(t);
+        }
         else{
             if(st.isEmpty()){
                 return false;
-                }
-                char p=st.pop();
-            if((p=='['&&t!=']')||(p=='{'&&t!='}')||(p=='('&&t!=')')){
-                return false;
-                }
-              }
             }
-            if(st.isEmpty())
-              {
-                return true;
-              }
-              else{
+            char p=st.pop();
+            if((p=='('&& t!=')')||(p=='[' && t!=']')||(p=='{'&& t!='}')){
                 return false;
-              }
-    }
+            }
+           
+        }
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    
+}
 }
 
         
